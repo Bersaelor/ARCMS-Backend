8e8ee7c6f39ea9697db4f845fd8b522a8501d5b2
Konrad Feiler <K_W_F@web.de>
2019-06-16 20:07:45 +0200

    attempt to add and call for dynamodb

--- user/user.js
+++ user/user.js
@@ -1,21 +1,43 @@
+/*jslint node: true */
+
 'use strict';
 
+const AWS = require('aws-sdk'); 
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 exports.get = async function(event, context, callback){
 
-    console.log("Event: ", event);
-
     var cognitoUserName = event.requestContext.authorizer.claims["cognito:username"];
-    const response = {
-        statusCode: 200,
-        headers: {
-            "x-custom-header" : "My Header Value"
-        },
-    body: JSON.stringify({ 
-            message: "Hello World!",
-            cognitoUserName: cognitoUserName
-        })
+
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        ProjectionExpression: "id, sk"
     };
 
-    callback(null, response);
-}
+    console.log("Scanning table");
+
+    const onScan = (err, data) => {
+        if (err) {
+            console.error('Scan failed to load data. Error JSON: ', JSON.stringify(err, null, 2));
+            callback(err);
+        } else {
+            console.log("Scan succeeded.");
+
+            const response = {
+                statusCode: 200,
+                headers: {
+                    "x-custom-header" : "My Header Value"
+                },
+            body: JSON.stringify({ 
+                    message: "Hello World!",
+                    cognitoUserName: cognitoUserName,
+                    items: data.Items
+                })
+            };
+        
+            return callback(null, response);
+        }
+    };
+
+    dynamoDb.scan(params, onScan);  
+};
