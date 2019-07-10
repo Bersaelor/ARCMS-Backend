1ac8be0ec17d1c1cf935280edd83c30d62d3b045
Konrad Feiler <K_W_F@web.de>
2019-07-10 20:03:13 +0200

    delete device endpoint

--- devices/devices.js
+++ devices/devices.js
@@ -22,6 +22,18 @@ async function loadDevicesFromDB(email, brand) {
     return dynamoDb.query(params).promise()
 }
 
+async function deleteDevice(email, id, brand) {
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        Key: {
+            "id": `${email}#device`,
+            "sk": `${brand}#${id}`,
+        } 
+    };
+
+    return dynamoDb.delete(params).promise()
+}
+
 function makeHeader(content) {
     return { 
         'Access-Control-Allow-Origin': '*',
@@ -40,6 +52,7 @@ function mapDBEntriesToOutput(brandName, items) {
     })
 }
 
+// Get all devices for the current user
 exports.all = async (event, context, callback) => {
     if (event.queryStringParameters.brand == undefined) {
         callback(null, {
@@ -64,7 +77,6 @@ exports.all = async (event, context, callback) => {
     try {
         const data = await loadDevicesFromDB(cognitoUserName, brand);
         const devices = mapDBEntriesToOutput(brand, data.Items)
-        console.log("Query succeeded, devices: ", devices)
 
         const response = {
             statusCode: 200,
@@ -78,7 +90,54 @@ exports.all = async (event, context, callback) => {
         const response = {
             statusCode: err.statusCode || 501,
             headers: makeHeader('text/plain'),
-            body: 'Couldn\'t fetch the brands because of ' + err,
+            body: 'Failed to fetch the brands because of ' + err,
+        };
+        callback(null, response);
+        return;
+    }
+};
+
+// Delete a device from the current user
+exports.delete = async (event, context, callback) => {
+    if (event.queryStringParameters.brand == undefined) {
+        callback(null, {
+            statusCode: 403,
+            headers: makeHeader('text/plain'),
+            body: `Missing query parameter 'brand'`,
+        });
+    }
+
+    const brand = event.queryStringParameters.brand;
+
+    if (!event.requestContext.authorizer) {
+        callback(null, {
+            statusCode: 403,
+            headers: makeHeader('text/plain'),
+            body: `Cognito Authorization missing`,
+        });
+    }
+
+    const cognitoUserName = event.requestContext.authorizer.claims["cognito:username"].toLowerCase();
+
+    const id = event.pathParameters.id
+
+    try {
+        const deletionResponse = await deleteDevice(cognitoUserName, id, brand);
+        console.log("Deletion succeeded, devices: ", deletionResponse)
+
+        const response = {
+            statusCode: 200,
+            headers: makeHeader('application/json'),
+            body: JSON.stringify({ "message: ": "Deletion of device " + id + " successful" })
+        };
+    
+        callback(null, response);
+    } catch(err) {
+        console.error('Query failed to load data. Error JSON: ', JSON.stringify(err, null, 2));
+        const response = {
+            statusCode: err.statusCode || 501,
+            headers: makeHeader('text/plain'),
+            body: 'Failed to delete device because of ' + err,
         };
         callback(null, response);
         return;
