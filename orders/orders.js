5e2d5c6fea75d92042575d96d76556bcec11da76
Konrad Feiler <K_W_F@web.de>
2019-07-11 12:30:39 +0200

    create all-orders and create-order endpoints

---
+++ orders/orders.js
@@ -0,0 +1,129 @@
+/*jslint node: true */
+
+'use strict';
+
+const AWS = require('aws-sdk'); 
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+async function loadOrdersFromDB(email, brand) {
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        ProjectionExpression: "id, sk, models",
+        KeyConditionExpression: "#id = :value",
+        ExpressionAttributeNames:{
+            "#id": "id",
+        },
+        ExpressionAttributeValues: {
+            ":value": `${email}#${brand}`
+        }
+    };
+
+    return dynamoDb.query(params).promise()
+}
+
+function makeHeader(content) {
+    return { 
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Content-Type': content
+    };
+}
+
+function mapDBEntriesToOutput(items) {
+    return items.map((value) => {
+        return {
+            date: value.sk,
+            store: value.id.slice(0, -7),
+            content: value.models
+        }
+    })
+}
+
+// Get all orders for the current user or brand depending on the accessLvl
+exports.all = async (event, context, callback) => {
+    if (event.queryStringParameters.brand == undefined) {
+        callback(null, {
+            statusCode: 403,
+            headers: makeHeader('text/plain'),
+            body: `Missing query parameter 'brand'`,
+        });
+    }
+
+    const brand = event.queryStringParameters.brand;
+
+    if (!event.requestContext.authorizer) {
+        callback(null, {
+            statusCode: 403,
+            headers: makeHeader('text/plain'),
+            body: `Cognito Authorization missing`,
+        });
+    }
+
+    const cognitoUserName = event.requestContext.authorizer.claims["cognito:username"].toLowerCase();
+
+    try {
+        const data = await loadOrdersFromDB(cognitoUserName, brand);
+        const orders = mapDBEntriesToOutput(data.Items)
+
+        const response = {
+            statusCode: 200,
+            headers: makeHeader('application/json'),
+            body: JSON.stringify(orders)
+        };
+    
+        callback(null, response);
+    } catch(err) {
+        console.error('Query failed to load data. Error JSON: ', JSON.stringify(err, null, 2));
+        const response = {
+            statusCode: err.statusCode || 501,
+            headers: makeHeader('text/plain'),
+            body: 'Failed to fetch the brands because of ' + err,
+        };
+        callback(null, response);
+        return;
+    }
+};
+
+// Create and save a new order
+exports.create = async (event, context, callback) => {
+    if (event.queryStringParameters.brand == undefined) {
+        callback(null, {
+            statusCode: 403,
+            headers: makeHeader('text/plain'),
+            body: `Missing query parameter 'brand'`,
+        });
+    }
+
+    const brand = event.queryStringParameters.brand;
+
+    if (!event.requestContext.authorizer) {
+        callback(null, {
+            statusCode: 403,
+            headers: makeHeader('text/plain'),
+            body: `Cognito Authorization missing`,
+        });
+    }
+
+    const cognitoUserName = event.requestContext.authorizer.claims["cognito:username"].toLowerCase();
+
+    try {
+        // TODO create order
+
+        const response = {
+            statusCode: 200,
+            headers: makeHeader('application/json'),
+            body: JSON.stringify({ "message: ": "Deletion of device " + id + " successful" })
+        };
+    
+        callback(null, response);
+    } catch(err) {
+        console.error('Query failed to load data. Error JSON: ', JSON.stringify(err, null, 2));
+        const response = {
+            statusCode: err.statusCode || 501,
+            headers: makeHeader('text/plain'),
+            body: 'Failed to delete device because of ' + err,
+        };
+        callback(null, response);
+        return;
+    }
+};
