7049b1e573bda65d5492cc056091b705839bd9d4
Konrad Feiler <K_W_F@web.de>
2019-09-23 11:19:19 +0200

    convert all grafix orders to new style

---
+++ orders/convert.js
@@ -0,0 +1,96 @@
+/*jslint node: true */
+
+'use strict';
+
+const AWS = require('aws-sdk'); 
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+async function loadAllOrdersFromDB(brand) {
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        ProjectionExpression: "id, sk, contact, orderJSON",
+        KeyConditionExpression: "#id = :value",
+        ExpressionAttributeNames:{
+            "#id": "id",
+        },
+        ExpressionAttributeValues: {
+            ":value": `${brand}#order`,
+        },
+        ScanIndexForward: false
+    };
+
+    return dynamoDb.query(params).promise()
+}
+
+function makeHeader(content) {
+    return { 
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Content-Type': content
+    };
+}
+
+function convertOrder(originalOrder) {
+    const brand = originalOrder.id.split('#')[0]
+    const email = originalOrder.sk.split('#')[0]
+    const timeString = originalOrder.sk.split('#')[1]
+
+    return {
+        "id": `${brand}#order#${email}`,
+        "sk": timeString,
+        "contact": originalOrder.contact,
+        "orderJSON": originalOrder.orderJSON
+    }
+}
+
+async function writeOrder(order) {
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        Item: order
+    };
+
+    return dynamoDb.put(params).promise();
+}
+
+// Convert all existing orders to new version structure in DynamoDB
+exports.convertAll = async (event, context, callback) => {
+
+    const brand = event.queryStringParameters.brand;
+
+    if (!brand) {
+        callback(null, {
+            statusCode: 403,
+            headers: makeHeader('text/plain'),
+            body: `brand query parameter missing`,
+        });
+        return;
+    }
+
+    try {
+        const data = await loadAllOrdersFromDB(brand)
+
+        const writePromises = data.Items.map((order) => {
+            const newOrder = convertOrder(order)
+            return writeOrder(newOrder)
+        })
+
+        const finishedWriting = Promise.all(writePromises)
+
+        const response = {
+            statusCode: 200,
+            headers: makeHeader('text/plain'),
+            body: `Successfully finished ${writePromises.length} writes`
+        };
+        callback(null, response);
+
+    } catch(err) {
+        console.error('Query failed to load data. Error JSON: ', JSON.stringify(err, null, 2));
+        const response = {
+            statusCode: err.statusCode || 501,
+            headers: makeHeader('text/plain'),
+            body: 'Failed to convert orders because of ' + err,
+        };
+        callback(null, response);
+        return;
+    }
+}
