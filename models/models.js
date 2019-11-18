b8bb2a5aeb17f086cd1144777cc48e9b062ea6fe
Konrad Feiler <K_W_F@web.de>
2019-11-19 00:59:09 +0100

    CRUD methods for models stubbed

--- models/models.js
+++ models/models.js
@@ -38,7 +38,7 @@ async function getAccessLvl(cognitoUserName, brand) {
     });
 }
 
-async function getCategorys(brand) {
+async function getModels(brand, category) {
     var params = {
         TableName: process.env.CANDIDATE_TABLE,
         ProjectionExpression: "sk, image, localizedTitles, localizedDetails",
@@ -71,24 +71,24 @@ async function getSignedImageUploadURL(key, type) {
     });
 }
 
-function convertStoredCategory(storedCategory) {
-    var category = storedCategory
-    category.name = storedCategory.sk
+function convertStoredModel(storedModel) {
+    var category = storedModel
+    category.name = storedModel.sk
     delete category.sk
-    category.localizedTitles = JSON.parse(storedCategory.localizedTitles)
-    category.localizedDetails = JSON.parse(storedCategory.localizedDetails)
-    category.image = "https://images.looc.io/" + storedCategory.image
+    category.localizedTitles = JSON.parse(storedModel.localizedTitles)
+    category.localizedDetails = JSON.parse(storedModel.localizedDetails)
+    category.image = "https://images.looc.io/" + storedModel.image
     return category
 }
 
-async function createCategoryInDB(values, brand) {
+async function createModelInDB(values, brand, category) {
     const sanitize = (value) => ( value ? value : "placeholder.png" ) 
 
     var params = {
         TableName: process.env.CANDIDATE_TABLE,
         Item: {
-            "id": `${brand}#category`,
-            "sk": values.name,
+            "id": `${brand}#model`,
+            "sk": `${category}#${values.name}`,
             "image": sanitize(values.image),
             "localizedTitles": values.localizedTitles ? JSON.stringify(values.localizedTitles) : "n.A.",
             "localizedDetails": values.localizedDetails ? JSON.stringify(values.localizedDetails) : "n.A."
@@ -98,12 +98,12 @@ async function createCategoryInDB(values, brand) {
     return dynamoDb.put(params).promise();
 }
 
-async function deleteCategoryFromDB(name, brand) {
+async function deleteModelFromDB(name, brand, category) {
     var params = {
         TableName: process.env.CANDIDATE_TABLE,
         Key: {
-            "id": `${brand}#category`,
-            "sk": name
+            "id": `${brand}#model`,
+            "sk": `${category}#${name}`
         } 
     };
 
@@ -130,14 +130,15 @@ const fileExtension = (filename) => {
 // Cached, public collections endpoint
 exports.all = async (event, context, callback) => {
     const brand = event.pathParameters.brand.toLowerCase()
+    const category = event.pathParameters.category.toLowerCase()
 
-    const data = await getCategorys(brand)
+    const data = await getModels(brand, category)
 
     const categories = data.Items.map((cat) => {
-        return convertStoredCategory(cat)
+        return convertStoredModel(cat)
     })
 
-    console.log("Returning ", categories.length, " categories from DynDB")
+    console.log("Returning ", categories.length, " models from DynDB")
 
     callback(null, {
         statusCode: 200,
@@ -149,6 +150,8 @@ exports.all = async (event, context, callback) => {
 exports.createNew = async (event, context, callback) => {
     const cognitoUserName = event.requestContext.authorizer.claims["cognito:username"].toLowerCase();
     const brand = event.pathParameters.brand.toLowerCase()
+    const category = event.pathParameters.category.toLowerCase()
+
     var body = JSON.parse(event.body)
 
     const imageUploadRequested = body.imageName
@@ -197,17 +200,17 @@ exports.createNew = async (event, context, callback) => {
             body.image = path
         }
 
-        const writeDBPromise = createCategoryInDB(body, brand)
+        const writeDBPromise = createModelInDB(body, brand, category)
 
         const imageUploadURL = imageURLPromise ? await imageURLPromise : undefined
         const writeSuccess = await writeDBPromise
-        console.log("write Category to db success: ", writeSuccess)
+        console.log("write model to db success: ", writeSuccess)
 
         const response = {
             statusCode: 200,
             headers: makeHeader('application/json' ),
             body: JSON.stringify({
-                "message": "Category creation or update successful",
+                "message": "Model creation or update successful",
                 "uploadURL": imageUploadURL ? imageUploadURL : ""
             })
         };
@@ -215,7 +218,7 @@ exports.createNew = async (event, context, callback) => {
         callback(null, response);
 
     } catch(error) {
-        console.error('Failed to create category: ', JSON.stringify(error, null, 2));
+        console.error('Failed to create model: ', JSON.stringify(error, null, 2));
         callback(null, {
             statusCode: error.statusCode || 501,
             headers: makeHeader('text/plain'),
@@ -229,24 +232,25 @@ exports.delete = async (event, context, callback) => {
     let cognitoUserName = event.requestContext.authorizer.claims["cognito:username"].toLowerCase();
     const brand = event.pathParameters.brand.toLowerCase()
     const id = event.pathParameters.id.toLowerCase()
+    const category = event.pathParameters.category.toLowerCase()
 
-    if (!id || !brand) {
+    if (!id || !brand || !category) {
         callback(null, {
             statusCode: 403,
             headers: makeHeader('text/plain'),
-            body: `Expected both a brand and a user-id, one is missing.`,
+            body: `Expected both a brand, modelname and a category, one is missing.`,
         });
         return;
     }
 
-    console.log(cognitoUserName, " wants to delete category named: ", id, " from brand ", brand)
+    console.log(cognitoUserName, " wants to delete model named: ", id, " from brand ", brand)
     try {
         // make sure the current cognito user has high enough access lvl
         const accessLvlPromise = getAccessLvl(cognitoUserName, brand);
 
         const ownAccessLvl = await accessLvlPromise;
         if (!accessLvlMayCreate(ownAccessLvl)) {
-            const msg = `User ${cognitoUserName} is not allowed to delete categories of ${brand}`
+            const msg = `User ${cognitoUserName} is not allowed to delete models of ${brand}`
             callback(null, {
                 statusCode: 403,
                 headers: makeHeader('application/json' ),
@@ -255,18 +259,18 @@ exports.delete = async (event, context, callback) => {
             return;
         }
 
-        const dbDeletionResponse = await deleteCategoryFromDB(id, brand)
+        const dbDeletionResponse = await deleteModelFromDB(id, brand, category)
         console.log("dbDeletionResponse: ", dbDeletionResponse)
 
         const response = {
             statusCode: 200,
             headers: makeHeader('application/json'),
-            body: JSON.stringify({ "message": "Deletion of category " + id + " successful" })
+            body: JSON.stringify({ "message": "Deletion of model " + id + " successful" })
         };
 
         callback(null, response);
     } catch (error) {
-        console.error('Query failed to load data. Error JSON: ', JSON.stringify(error, null, 2));
+        console.error('Query failed to delete. Error JSON: ', JSON.stringify(error, null, 2));
         callback(null, {
             statusCode: error.statusCode || 501,
             headers: makeHeader('text/plain'),
