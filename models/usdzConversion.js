e2bafc6a0ec3632c9ec5bc1061b35eca5b9c7034
Konrad Feiler <K_W_F@web.de>
2020-01-07 15:15:37 +0100

    use larger instances for larger files, so RAM isn't running out

--- models/usdzConversion.js
+++ models/usdzConversion.js
@@ -5,7 +5,7 @@
 const AWS = require('aws-sdk'); 
 const ec2 = new AWS.EC2();
 
-function startInstance(file) {
+function startInstance(file, sizeInMB) {
     const init_script = `
     #!/bin/bash
     /usr/bin/aws s3 cp s3://ec2-looc-storage/ec2-init.sh /tmp/ 
@@ -13,10 +13,18 @@ function startInstance(file) {
     /tmp/ec2-init.sh ${ file }
     `
     const base64Script = Buffer.from(init_script).toString('base64')
+    var instanceType
+    if (sizeInMB > 40) {
+        instanceType = "t2.medium"
+    } else if (sizeInMB > 15) {
+        instanceType = "t2.small"
+    } else {
+        instanceType = "t2.micro"
+    }
 
     var params = {
         ImageId: "ami-0d4c3eabb9e72650a",
-        InstanceType: "t2.micro",
+        InstanceType: instanceType,
         KeyName: "Convert3DEC2Pair",
         MaxCount: 1,
         MinCount: 1,
@@ -40,11 +48,14 @@ exports.convert = async (event, context, callback) => {
     for (const index in event.Records) {
         const record = event.Records[index]
         const key = record.s3.object.key
+        const sizeInMB = record.s3.object.size / (1024 * 1024)
+
+        console.log("sizeInMB: ", sizeInMB)
 
         console.log(`${key} was added to models bucket, starting EC2 instance to convert it to usdz`)
 
         try {
-            const response = await startInstance(key)
+            const response = await startInstance(key, sizeInMB)
             console.log("Success: ", response.Instances, " Instances created")
         } catch(error) {
             console.log("Failed: ", error)
