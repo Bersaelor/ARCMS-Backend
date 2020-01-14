f893b30f7a5eb5baa75e8adb83983ca9a5fd6a74
Konrad Feiler <K_W_F@web.de>
2020-01-14 22:55:32 +0100

    fetch all models and categories

--- cleanup/cleanup.js
+++ cleanup/cleanup.js
@@ -17,24 +17,59 @@ function makeHeader(content) {
     };
 }
 
+function fetchAllModels(brands) {
+    let modelFetches = brands.map(brand => {
+        return getAllModels(brand).then(data => {
+            return data.Items.map(model => {
+                return {
+                    'image': model.image,
+                    'modelFile': model.modelFile,
+                    'usdzFile': model.usdzFile
+                }
+            })
+        })
+    })
+    return Promise.all(modelFetches)
+} 
+
+function fetchAllCategories(brands) {
+    let categoryFetches = brands.map ( brand => {
+        return getCategorys(brand).then(data => {
+            return data.Items.map(category => category.image)
+        })
+    })
+    return Promise.all(categoryFetches)
+}
+
 // Delete images and models from S3 that are no longer used for any dynamodb entities
 exports.cleanOldModelsAndImages = async (event, context, callback) => {
     const brands = Object.keys(brandSettings)
 
     try {
-        
         console.log("Fetching Models and Categories for: ", brands)
-        let modelFetches = brands.map ( brand => getAllModels(brand))
-        let modelsPromise = Promise.all(modelFetches)
+        let modelsPromise = fetchAllModels(brands)
+        let categoryPromise = fetchAllCategories(brands)
 
-        let categoryFetches = brands.map ( brand => getCategorys(brand))
-        let categoryPromise = Promise.all(categoryFetches)
+        let currentImages = new Set()
+        let currentModelFiles = new Set()
 
         let models = await modelsPromise
+        models.forEach(array => {
+            array.forEach(model => {
+                currentImages.add(model.image)
+                currentModelFiles.add(model.modelFile)
+                currentModelFiles.add(model.usdzFile)
+            })
+        })
+
         let categories = await categoryPromise
-        console.log("models ", models)
-        console.log("categories ", categories)
-        
+        categories.forEach(array => {
+            array.forEach(image => currentImages.add(image))
+        })
+
+        console.log("currentImages ", currentImages)
+        console.log("currentModelFiles ", currentModelFiles)
+
         callback(null, {
             statusCode: 200,
             headers: makeHeader('text/plain'),
@@ -42,7 +77,7 @@ exports.cleanOldModelsAndImages = async (event, context, callback) => {
         });
     
     } catch(err) {
-        console.error('Failed cleaning up. Error JSON: ', JSON.stringify(err, null, 2));
+        console.error('Failed cleaning up. Error : ', err);
         const response = {
             statusCode: err.statusCode || 501,
             headers: makeHeader('text/plain'),
