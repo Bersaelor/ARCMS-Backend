64ad71331df5f51b69ed0732c6d200483af32136
Konrad Feiler <K_W_F@web.de>
2021-02-11 16:21:34 +0100

    set the uploaded glb as modelFile if it is newer or no modelFile is set

--- conversion-service/models/usdzConversion.js
+++ conversion-service/models/usdzConversion.js
@@ -4,6 +4,8 @@
 
 const AWS = require('aws-sdk'); 
 const ec2 = new AWS.EC2();
+const path = require('path');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 function startInstance(file, sizeInMB) {
     const init_script = `
@@ -45,19 +47,84 @@ function startInstance(file, sizeInMB) {
     })
 }
 
+async function getModel(brand, category, id) {
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        ProjectionExpression: "sk, modelFile",
+        KeyConditionExpression: "#id = :value and #sk = :searchKey",
+        ExpressionAttributeNames:{
+            "#id": "id",
+            "#sk": "sk"
+        },
+        ExpressionAttributeValues: {
+            ":value": `${brand}#model`,
+            ":searchKey": `${category}#${id}`
+        },
+    };
+
+    return dynamoDb.query(params).promise()
+}
+
+async function updateModel(key, value, name, brand, category) {
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        Key: {id: `${brand}#model`, sk: `${category}#${name}` },
+        UpdateExpression: `set ${key} = :value`,
+        ExpressionAttributeValues: {
+            ':value' : value,
+        },
+    };
+
+    return dynamoDb.update(params).promise()
+}
+
+// Check the dynamoDB for the model entry, if no modelFile value is 
+function checkAndUpdateDBIfNecessary(key) {
+    const parsedPath = path.parse(key)
+    const fileName = parsedPath.name
+    const brand = key.split('/')[1]
+    const category = key.split('/')[2]
+    const dashSeparated = parsedPath.name.split('-')
+    const timestamp = dashSeparated.pop() // pop the timestamp
+    const modelId = dashSeparated.join('-')
+
+    // load the model entry and check whether the model file name is older then the *.glb file
+    // if the glb file is converted from a modelFile dae, then it would have an equal timestring
+    
+    return getModel(brand, category, modelId).then ( result => {
+        if (!result || !result.Items || result.Items.length == 0) {
+            throw `Failed to find model with brand: ${brand}, category: ${category}, modelId: ${modelId} in DB`
+        }
+        const model = result.Items[0];
+        if (model.modelFile) {
+            const existingTimestamp = path.parse(model.modelFile).name.split('-').pop();
+            if (existingTimestamp < timestamp) {
+                console.log("Existing modelFile of " + modelId + " was older then " + fileName + " so overwriting modelFile"); 
+                return updateModel("modelFile", key, modelId, brand, category)
+            }
+        } else {
+            console.log("Existing " + modelId + " has no modelFile entry, updating to " + fileName); 
+            return updateModel("modelFile", key, modelId, brand, category)
+        }
+        return "No update necessary"
+    });
+}
+
+// Convert glb files deposited into s3/original to usdz, but also create an encrypted glb copy
 exports.convert = async (event, context, callback) => {
     for (const index in event.Records) {
         const record = event.Records[index]
         const key = record.s3.object.key
         const sizeInMB = record.s3.object.size / (1024 * 1024)
 
-        console.log("sizeInMB: ", sizeInMB)
-
         console.log(`${key} was added to models bucket, starting EC2 instance to convert it to usdz`)
 
         try {
-            const response = await startInstance(key, sizeInMB)
-            console.log("Success: ", response.Instances, " Instances created")
+            const response = await Promise.all([
+                startInstance(key, sizeInMB),
+                checkAndUpdateDBIfNecessary(key)
+            ])
+            console.log("Success: ", response[0].Instances, " Instances created and DB update Response: " + response[1])
         } catch(error) {
             console.log("Failed: ", error)
             return callback(error)
